cfc3be0b322ff92be65165c87d84111c20e5f20a
Stéphane Goetz <onigoetz@onigoetz.ch>
2018-09-20 20:17:51 +0200

    Remove HHVM specific code

--- libs/Console/Serve.php
+++ libs/Console/Serve.php
@@ -47,14 +47,6 @@ class Serve extends DauxCommand
 
         echo "Daux development server started on http://{$host}:{$port}/\n";
 
-        if (defined('HHVM_VERSION')) {
-            if (version_compare(HHVM_VERSION, '3.8.0') >= 0) {
-                passthru("{$binary} -m server -v Server.Type=proxygen -v Server.SourceRoot={$base}/ -v Server.IP={$host} -v Server.Port={$port} -v Server.DefaultDocument=server.php -v Server.ErrorDocument404=server.php");
-            } else {
-                throw new Exception("HHVM's built-in server requires HHVM >= 3.8.0.");
-            }
-        } else {
-            passthru("{$binary} -S {$host}:{$port} {$base}/index.php");
-        }
+        passthru("{$binary} -S {$host}:{$port} {$base}/index.php");
     }
 }
