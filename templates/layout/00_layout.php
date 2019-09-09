75f34cae30b4ffb3fea0e1be28c37aa36674dbc9
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-09-09 19:56:14 +0200

    Put search field in separate template

--- templates/layout/00_layout.php
+++ templates/layout/00_layout.php
@@ -61,43 +61,7 @@
         echo '<script src="' . $js . '"></script>';
     } ?>
 
-    <?php if ($params['html']['search']) { ?>
-        <script>
-            <?php
-            $search_strings = [
-                "Search_one_result",
-                "Search_results",
-                "Search_no_results",
-                "Search_common_words_ignored",
-                "Search_too_short",
-                "Search_one_character_or_more",
-                "Search_should_be_x_or_more",
-                "Link_previous",
-                "Link_next",
-            ];
-            $search_translations = [];
-            foreach($search_strings as $key) {
-                $search_translations[$key] = $this->translate($key);
-            }
-            ?>
-
-            window.searchLanguage = <?= json_encode($page['language']) ?>;
-            window.searchTranslation = <?= json_encode($search_translations) ?>;
-        </script>
-
-        <!-- Tipue Search -->
-        <script type="text/javascript" src="<?php echo $base_url; ?>tipuesearch/jquery-1.11.3.min.js"></script>
-        <script type="text/javascript" src="<?php echo $base_url; ?>tipuesearch/tipuesearch.js"></script>
-
-        <script>
-            window.onunload = function(){}; // force $(document).ready to be called on back/forward navigation in firefox
-            $(function() {
-                tipuesearch({
-                    'base_url': '<?php echo $base_url?>'
-                });
-            });
-        </script>
-    <?php } ?>
+    <?php $this->insert('theme::partials/search_script', ['page' => $page, 'base_url' => $base_url]); ?>
 
 </body>
 </html>
