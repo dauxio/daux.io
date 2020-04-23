67d5e9a3193ab9f4e5c30ef0573899b150d525ec
Stéphane Goetz <onigoetz@onigoetz.ch>
2020-04-23 22:38:58 +0200

    Disable report path fixing to test how Sonar-scanner reacts

--- scripts/fix_reports.sh
+++ scripts/fix_reports.sh
@@ -2,6 +2,6 @@
 
 # https://community.sonarsource.com/t/code-coverage-doesnt-work-with-github-action/16747/5
 echo "fix code coverage paths"
-sed -i 's/\/home\/runner\/work\/daux.io\/daux.io\//\/github\/workspace\//g' coverage.clover
+#sed -i 's/\/home\/runner\/work\/daux.io\/daux.io\//\/github\/workspace\//g' coverage.clover
 
 php scripts/fix_report.php
