c3863e11abfc840e79836260c637a22b09c77c9b
Stéphane Goetz <onigoetz@onigoetz.ch>
2022-01-28 23:12:45 +0100

    Make sure permalinks are unique in singlepage

--- libs/Format/HTMLFile/ContentTypes/Markdown/CommonMarkConverter.php
+++ libs/Format/HTMLFile/ContentTypes/Markdown/CommonMarkConverter.php
@@ -1,9 +1,23 @@
 <?php namespace Todaymade\Daux\Format\HTMLFile\ContentTypes\Markdown;
 
+use League\CommonMark\Normalizer\UniqueSlugNormalizerInterface;
 use Todaymade\Daux\Config;
 
 class CommonMarkConverter extends \Todaymade\Daux\Format\HTML\ContentTypes\Markdown\CommonMarkConverter
 {
+    /**
+     * Create a new Markdown converter pre-configured for CommonMark
+     *
+     * @param array<string, mixed> $config
+     */
+    public function __construct(array $config = [])
+    {
+        // As we are using a single page, we must make sure permalinks don't collide
+        $config['slug_normalizer']['unique'] = UniqueSlugNormalizerInterface::PER_ENVIRONMENT;
+
+        parent::__construct($config);
+    }
+
     protected function getLinkRenderer(Config $config)
     {
         return new LinkRenderer($config);
