06cf2be0114a2045d2fab46b644994c904561051
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2022-12-11 11:33:15 +0100

    use relative paths

--- scripts/fix_reports.sh
+++ scripts/fix_reports.sh
@@ -3,6 +3,6 @@
 # https://community.sonarsource.com/t/code-coverage-doesnt-work-with-github-action/16747/5
 echo "fix code coverage paths"
 sed -i 's@'$GITHUB_WORKSPACE'@/github/workspace/@g' coverage.clover
-sed -i 's@'$GITHUB_WORKSPACE'@/github/workspace@g' test-report.xml
+sed -i 's@'$GITHUB_WORKSPACE'@@g' test-report.xml
 
 #php scripts/fix_report.php
